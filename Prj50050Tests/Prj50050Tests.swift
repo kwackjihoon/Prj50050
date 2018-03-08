26066d06dde0a30d81b341cf975f1873d509fee4
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-03-08 10:28:24 +0900

    Initial Commit

---
+++ Prj50050Tests/Prj50050Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Prj50050Tests.swift
+//  Prj50050Tests
+//
+//  Created by D7703_25 on 2018. 3. 8..
+//  Copyright © 2018년 김종현. All rights reserved.
+//
+
+import XCTest
+@testable import Prj50050
+
+class Prj50050Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
